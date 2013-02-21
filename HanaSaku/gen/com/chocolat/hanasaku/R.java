2509b8486848e77f0182aa30e7009822a5ddf419
chocoffice <shiori.yamazaki.1002@gmail.com>
2013-02-21 12:21:35 +0900

    レビュー受ける前

--- HanaSaku/gen/com/chocolat/hanasaku/R.java
+++ HanaSaku/gen/com/chocolat/hanasaku/R.java
@@ -15,11 +15,10 @@ public final class R {
         public static final int butterfly_blue=0x7f020001;
         public static final int butterfly_green=0x7f020002;
         public static final int butterfly_red=0x7f020003;
-        public static final int dummy_flower=0x7f020004;
-        public static final int flower_blue=0x7f020005;
-        public static final int flower_green=0x7f020006;
-        public static final int flower_red=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
+        public static final int flower_blue=0x7f020004;
+        public static final int flower_green=0x7f020005;
+        public static final int flower_red=0x7f020006;
+        public static final int ic_launcher=0x7f020007;
     }
     public static final class id {
         public static final int menu_settings=0x7f070000;
